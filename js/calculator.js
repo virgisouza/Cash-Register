acb41ba5bdba280df26844edb44c2b21d1ac491e
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-08-31 14:38:51 -1000

    second commit working on display at onKey numbers

--- js/calculator.js
+++ js/calculator.js
@@ -2,8 +2,9 @@
 
 var calculator =(function(){
 
-	var total = 0;
+	var total = 0.00;
 	var memory = 0;
+	var disArr = [];
 
 function add(num){
 	total += num;
@@ -25,9 +26,26 @@ function multiply(num){
 	return total;
 }
 
-function display(){
-
+function equalsTo(num){
+	//initialize function operation in display or memory
 }
+
+function display(num){
+	disArr.push(num); //when onKey event is initialized, number data will get pushed into empty array (disArr)
+	if(disArr.length >= 2){ //combine number elements in array into one number value ([1,2,3,4] === [1234])
+		var number = disArr.join('');
+			//parseInt(disArr);
+			//console.log(disArr)
+	} 
+	return number; // [display] will hold disArr element
+};
+
+//cash_register.js wil initialize diplay function as each number has onKey event
+display(4)
+display(5)
+display(6)
+display(7)
+
 
 function clear(){
 	//will clear out display
@@ -41,7 +59,8 @@ function depositCash(){
 	//add the amount currently in the [display] to the cash reg, then clears the display
 }
 
-function withdrawCash(){
+function withdrawCash(arr){
+
 	//will remove the amount currently in the [display] to the cash reg, then clears the display
 }
 
@@ -51,6 +70,7 @@ return {
 	subtract: subtract,
 	divide: divide,
 	multiply:multiply,
+	equalsTo:equalsTo,
 	display: display,
 	clear: clear,
 	getBalance: getBalance,
--- js/cash_register.js
+++ js/cash_register.js
@@ -1,4 +1,11 @@
-console.log('sanity check cash_register.js');
+
+//shows numbers on display screen 
+var regDisplay = document.getElementById('display');
+var displayNum = regDisplay.innerHTML;
+regDisplay.innerHTML = calculator.display();
+
+//add event listener to keyboard to initialize display function with correct umber showing.
+
 
 /*
 ## cash_register.js
@@ -6,5 +13,11 @@ console.log('sanity check cash_register.js');
 Write the rest of the logic to perform the necessary functions to achieve the goal.  
 You may create modules within `cash_register.js` in order to separate concerns.  
 For example, the function of updating the `( display )` should be handled in one place, or one module, instead of interweaved throughout the spaghetti.  
+
+var newRanking = document.getElementById("rating1");
+var newRankingText = newRanking.innerHTML;
+newRanking.innerHTML = "Rating: 5 Stars";
+console.log(newRanking);
+
 
 */
