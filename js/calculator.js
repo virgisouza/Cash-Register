e28aa94ffcdac72dbafb03a5564bfae83ee5de56
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-09-06 15:41:45 -1000

    update operations, using eval()

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,16 @@
 
 
-var calculator =(function(){
+var calculator = (function(){
 
-	var total = 0.00;
-	var memory = [];
-	var disArr = [];
+	var total = 0;
+	var memory = 0;
+	var displayArr = [];
+	var resultNum  = 0;
 
-function add(num1, num2){
-	num1 = memory;
-	console.log(num1)
-	total = num1 + num2;
+	
+
+function add(num){
+	total += num;
 	return total;
 }
 
@@ -28,46 +29,59 @@ function multiply(num1, num2){
 	return total;
 }
 
-function equalsTo(num){
+	//evaluate function operation in 'display'
+function equalsTo(operator){
+	var result = eval(document.getElementById('display').innerHTML);
+	console.log(result)
+	return result;
 
-	return total;
-	//initialize function operation 
 }
 
 function display(num){
-	disArr.push(num);    
-	var number = disArr.join(''); 
-	memory.push(number);
-	return number;
+	//displayArr.push(num);  
+	//var numbers = parseInt(displayArr.join(''));
+	//saveMemory(numbers);
+	//return numbers;
 }
 
+function recallMemory(){
+  return memory;
+}
 
+  /**
+   * Stores the value of `total` to `memory`
+   */
+function saveMemory(num){
+  memory = num;
+  return memory;
+}
+
+  /**
+   * Clear the value stored at `memory`
+   */
 function clear(){
-	memory = 0;
-	disArr.push(memory);
-	var clearedOut = disArr.pop();
-	return clearedOut;   //will clear out display and show 0.
+  return '';
 }
 
 
-function getBalance(){  //will display current balance
-	if(memory.length > 0){
-	var newMem = memory.pop();
-	return newMem;
-}else{
+function getBalance(){ 
+	if(memory > 0){
+ console.log(memory)
 	return memory;
+}else{
+	return 0;
 	}
 }
 
 function depositCash(num){
 	memory = total + num;
-	return disArr;
+	return displayArr;
 	//add the amount currently in the [display] to the cash reg, then clears the display
 }
 
 function withdrawCash(num){
 	memory = total - num;
-	return disArr;
+	return displayArr;
 	//will remove the amount currently in the [display] to the cash reg, then clears the display
 }
 
@@ -88,30 +102,3 @@ return {
 
 })();
 
-/*
-## Goal
-
-Create an html5 single page app that looks like a cash register with these buttons. The cash register starts with `$0.00` funds
-
-```
-[________________] <-----( display )
-
-[7] [8]  [9]   [÷]  [clear]
-[4] [5]  [6]   [×]  [get balance]
-[1] [2]  [3]   [-]  [deposit cash]
-[0] [00] [.]   [+]  [withdraw cash]
-               [=]
-
-```
-
-`( display )` is the primary ui that prints all results  
-`[clear]` will clear the display  
-`[get balance]` will display the current balance  
-`[deposit cash]` will add the amount currently in the `( display )` to the cash register, then clears the display  
-`[withdraw cash]` will remove the amount currently in the `( display )` to the cash register, then clears the display  
-
-## calculator.js
-
-Write all calculator logic in `calculator.js` using the [revealing module pattern](http://slides.com/jasonsewell/object-literals-and-ze-module-pattern)  
-The module should be named `Calculator` and should only be concerned with performing mathematical computation. 
-*/
--- js/cash_register.js
+++ js/cash_register.js
@@ -3,103 +3,104 @@
 
 //00
 document.getElementById('digit00').addEventListener('click', function(event){
-	var digit00 = document.getElementById('display');
-	digit00.innerHTML = calculator.display(00);
+	document.getElementById('display').innerHTML += this.value;
 	console.log(00);
 }, true);
 
 //0
 document.getElementById('digit0').addEventListener('click', function(event){
-	var digit0 = document.getElementById('display');
-	digit0.innerHTML = calculator.display(0);
+	document.getElementById('display').innerHTML += this.value;
 	console.log(0);
 }, true);
 
 //1
 document.getElementById('digit1').addEventListener('click', function(event){
-	var digit1 = document.getElementById('display');
-	digit1.innerHTML = calculator.display(1);
+	document.getElementById('display').innerHTML += this.value;
 	console.log(1);
 }, true);
 
 //2
 document.getElementById('digit2').addEventListener('click', function(event){
-	var digit2 = document.getElementById('display');
-	digit2.innerHTML = calculator.display(2);
+	document.getElementById('display').innerHTML += this.value;
 	console.log(2);
 }, true);
 
 //3
 document.getElementById('digit3').addEventListener('click', function(event){
-	var digit3 = document.getElementById('display');
-	digit3.innerHTML = calculator.display(3);
+	document.getElementById('display').innerHTML += this.value;
 	console.log(3);
 }, true);
 
 //4
 document.getElementById('digit4').addEventListener('click', function(event){
-	var digit4 = document.getElementById('display');
-	digit4.innerHTML = calculator.display(4);
+	document.getElementById('display').innerHTML += this.value;
 	console.log(4);
 }, true);
 
 //5
 document.getElementById('digit5').addEventListener('click', function(event){
-	var digit5 = document.getElementById('display');
-	digit5.innerHTML = calculator.display(5);
+	document.getElementById('display').innerHTML += this.value;
 	console.log(5);
 }, true);
 
 //6
 document.getElementById('digit6').addEventListener('click', function(event){
-	var digit6 = document.getElementById('display');
-	digit6.innerHTML = calculator.display(6);
+	document.getElementById('display').innerHTML += this.value;
 	console.log(6);
 }, true);
 
 //7
 document.getElementById('digit7').addEventListener('click', function(event){
-	var digit7 = document.getElementById('display');
-	digit7.innerHTML = calculator.display(7);
+	document.getElementById('display').innerHTML += this.value;
 	console.log(7);
 }, true);
 
+
 //8
 document.getElementById('digit8').addEventListener('click', function(event){
-	var digit8 = document.getElementById('display');
-	digit8.innerHTML = calculator.display(8);
+	document.getElementById('display').innerHTML += this.value;
 	console.log(8);
 }, true);
 
 //9
 document.getElementById('digit9').addEventListener('click', function(event){
-	var digit9 = document.getElementById('display');
-	digit9.innerHTML = calculator.display(9);
+	document.getElementById('display').innerHTML += this.value;
 	console.log(9);
+}, true);
+
+//decimal
+document.getElementById('digit.').addEventListener('click', function(event){
+	document.getElementById('display').innerHTML += this.value;
 }, true);
 
 //operations event listeners
 
 //add
 document.getElementById('addOperation').addEventListener('click', function(event){
-	document.getElementById('display').innerHTML = calculator.add();
-	console.log('+');
+	document.getElementById('display').innerHTML += this.value;
 }, true);
 
 //subtract
 document.getElementById('subtractOperation').addEventListener('click', function(event){
-	document.getElementById('display').innerHTML = calculator.subtract();
+	document.getElementById('display').innerHTML += this.value;
 	console.log('-'); 
 }, true);
 
 //divide
-//document.getElementById('divideOperation').addEventListener('click', function(event){
-
-//}, true);
+document.getElementById('divideOperation').addEventListener('click', function(event){
+	document.getElementById('display').innerHTML += this.value;
+}, true);
 
 //multiply
+document.getElementById('multiplyOperation').addEventListener('click', function(event){
+	document.getElementById('display').innerHTML += this.value;
+}, true);
 
 //equalsTo
+document.getElementById('equalsToOperation').addEventListener('click', function(event){
+	document.getElementById('display').innerHTML = calculator.equalsTo();
+	console.log('equalsTo')
+}, true);
 
 //clear
  document.getElementById('clearButton').addEventListener('click',function (event){
